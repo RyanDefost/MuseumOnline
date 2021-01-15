5febf2cff4108f79a32fb3d1d81e7b9a0889458b
Ryan de Fost <ryandefost@hotmail.com>
2021-01-15 10:29:01 +0100

    Tussen commit webxr

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ controls.enableMovement();
 
 // create a model
 const melkMeisje = new Model('assets/models/vermeer/export_museumprops.babylon.gltf');
-melkMeisje.setPosition(-6.396, 0.104, -8.750);
+melkMeisje.setPosition(-9.057, 0.104, -0.139);
 melkMeisje.setScale(100, 100, 100);
-melkMeisje.setRotation(0, 45.45, 0);
+melkMeisje.setRotation(0.000, 153.27, 0.000);
 
 // create a model
 const cuberoom = new Model('assets/models/cube-room/cube-room.gltf');
 cuberoom.setPosition(-1.827, 0.000, -0.427);
-cuberoom.setScale(-2.636, 0.690, 1.485);
-cuberoom.setRotation(-0.00, -36.95, 0);
+cuberoom.setScale(-2.636, 14.720, 1.485);
+cuberoom.setRotation(0.0, -36.950, 0.0);
 
 // create some text
 const lamText = new Text('Lisse Art Museum');
-lamText.setPosition(-0.1, 2.2, -2.9);
-lamText.setFontsize(10);
+lamText.setPosition(-2.241, 5.149, 8.426);
+lamText.setFontsize(50);
 lamText.setScale(0.5, 0.5, 0.5)
-lamText.setRotation(0, -38, 0);
+lamText.setRotation(0, -218, 0);
 
-const lamText1 = new Text('Asdfghj');
-lamText1.setPosition(-0.1, 1.2, -2.9);
-lamText1.setFontsize(10);
-lamText1.setScale(0.5, 0.5, 0.5)
-lamText1.setRotation(0, -38, 0);
+//Folkert text
+const lamText1 = new Text('Folkert de Jong (Schilder)');
+lamText1.setPosition(-0.789, 1.189, 8.107);
+lamText1.setRotation(-0.76, -216.95, 0);
+lamText1.setScale(2.5, 2.5, 2.5);
 
 //Backdrop
 const Backdrop = new XRImage('assets/images/board.jpg');
@@ -39,7 +39,11 @@ Backdrop.setScale(15.911, 3.501, 1.488);
 // Floor.setRotation(-90.00, -36.95, 0);
 // Floor.setScale(35.611, 15.245, 1.5);
 
-
+//Folkert de Jong
+const Folkert = new XRImage('assets/fotos_kunstwerk_en_atelier_Folkert/Folkert de Jong portret.jpg');
+Folkert.setPosition(-1.508, 2.510, 8.107);
+Folkert.setRotation(-0.76, -36.95, 0);
+Folkert.setScale(2.389, 2.389, 2.389);
 
 // create an image
 const image1 = new XRImage('assets/images/Lisse-049.jpg');
@@ -49,21 +53,21 @@ image1.setScale(1.5, 1.5, 1.5);
 
 // create an image
 const image2 = new XRImage('assets/images/FolkertdeJong.jpg');
-image2.setPosition(4.139, 1.900, -3.642);
-image2.setRotation(0, -39, -1);
-image2.setScale(1.5, 1.5, 1.5);
+image2.setPosition(-5.701, 9.191, 5.545);
+image2.setRotation(-0.76, -36.95, 0);
+image2.setScale(6.445, 6.445, 6.445);
 
 // create an image
 const image3 = new XRImage('assets/images/Lisse-043.jpg');
-image3.setPosition(7.828, 1.900, 5.017);
+image3.setPosition(9.233, 5.459, 6.483);
 image3.setRotation(0, -127, 0);
-image3.setScale(1.5, 1.5, 1.5);
+image3.setScale(5.456, 5.456, 5.456);
 
 // create an image
 const image4 = new XRImage('assets/fotos_kunstwerk_en_atelier_Folkert/1c450880dcd514ee-jc_130925_MIDD_FDJ_02.jpg');
-image3.setPosition(7.828, 1.900, 5.017);
-image3.setRotation(0, -127, 0);
-image3.setScale(1.5, 1.5, 1.5);
+image4.setPosition(-9.398, 5.850, -8.723);
+image4.setRotation(0, -127, 0);
+image4.setScale(3.276, 3.276, 3.276);
 
 
 // create a 360 image
